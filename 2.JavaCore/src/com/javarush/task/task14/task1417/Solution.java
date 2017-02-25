7242d03ad037639d109a9afcd1efd422402105b1
VladimirShevel <v.shevel@gmail.com>
2017-02-26 01:58:15 +0200

    26.02.17 01:58

--- 2.JavaCore/src/com/javarush/task/task14/task1417/Solution.java
+++ 2.JavaCore/src/com/javarush/task/task14/task1417/Solution.java
@@ -10,6 +10,9 @@ import java.util.List;
 public class Solution {
     public static void main(String[] args) {
         Person ivan = new Person("Иван");
+//        ivan.allMoney.add(new Hrivna(3200));
+//        ivan.allMoney.add((new USD(500)));
+//        ivan.allMoney.add(new Ruble(0));
         for (Money money : ivan.getAllMoney()) {
             System.out.println(ivan.name + " имеет заначку в размере " + money.getAmount() + " " + money.getCurrencyName());
         }
@@ -17,14 +20,18 @@ public class Solution {
 
     static class Person {
         public String name;
+        private List<Money> allMoney;
 
         Person(String name) {
             this.name = name;
             this.allMoney = new ArrayList<Money>();
+            allMoney.add(new Hrivna(3200));
+            allMoney.add((new USD(500)));
+            allMoney.add(new Ruble(33.90d));
             //напишите тут ваш код
         }
 
-        private List<Money> allMoney;
+
 
         public List<Money> getAllMoney() {
             return allMoney;
