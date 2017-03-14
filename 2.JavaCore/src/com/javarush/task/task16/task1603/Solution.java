35f57bea071ad31349dcdd9628cf9275527da76d
User <v.shevel@gmail.com>
2017-03-14 18:58:55 +0200

    14.03.2017  18:58

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1603/Solution.java
@@ -0,0 +1,31 @@
+package com.javarush.task.task16.task1603;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/* 
+Список и нити
+*/
+
+public class Solution {
+    public static volatile List<Thread> list = new ArrayList<Thread>(5);
+
+    public static void main(String[] args) {
+        //SpecialThread specialTread = new SpecialThread();
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        list.add(new Thread(new SpecialThread()));
+        //Add your code here - добавьте свой код тут
+        for (Thread x : list){
+            System.out.println(x);
+            x.run();
+    }}
+
+    public static class SpecialThread implements Runnable {
+        public void run() {
+            System.out.println("it's a run method inside SpecialThread");
+        }
+    }
+}
