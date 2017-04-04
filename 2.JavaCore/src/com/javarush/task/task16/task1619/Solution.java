2522d3faf4207ab8e1e8a46c39dcbb2b3a7441f8
VladimirShevel <v.shevel@gmail.com>
2017-04-04 22:57:06 +0300

    04.04.2017

---
+++ 2.JavaCore/src/com/javarush/task/task16/task1619/Solution.java
@@ -0,0 +1,32 @@
+package com.javarush.task.task16.task1619;
+
+/* 
+А без interrupt слабо?
+*/
+
+public class Solution {
+
+    public static void main(String[] args) throws InterruptedException {
+        Thread t = new Thread(new TestThread());
+        t.start();
+        Thread.sleep(3000);
+        ourInterruptMethod();
+    }
+
+    public static void ourInterruptMethod() {
+        TestThread.ourInterrupt = false;
+    }
+
+    public static class TestThread implements Runnable {
+        public static boolean ourInterrupt=true;
+            public void run() {
+            while (ourInterrupt) {
+                try {
+                    System.out.println("he-he");
+                    Thread.sleep(500);
+                } catch (InterruptedException e) {
+                }
+            }
+        }
+    }
+}
